71e0d8855285c3a4838220e21a1fdb8555c82714
armanot@gmail.com <armanot@gmail.com>
2024-01-21 18:48:47 +0800

    change greeting

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello armanot! Good Day to you"
+    return "Hello armanot! Salam.."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
