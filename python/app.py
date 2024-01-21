86de1df3f8957e55f1c5a621bed2f404e4ff3d08
armanot@gmail.com <armanot@gmail.com>
2024-01-21 18:36:04 +0800

    second commit

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello armanot! Good Day"
+    return "Hello armanot! Good Day to you"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
