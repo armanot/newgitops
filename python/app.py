0b03967f1c2c67bee07dba89371cfa2d37c15b2a
armanot <34114374+armanot@users.noreply.github.com>
2024-01-21 20:09:06 +0800

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello armanot! Salam..How are you?"
+    return "Hello armanot! Salam..Testing 1"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8000)
